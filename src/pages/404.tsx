5c83e5cdd1c01b2191a01e64606fa9e36bcd5ab0
Gypsophila <onsummer@foxmail.com>
2021-07-27 18:03:29 +0800

    [update] 404page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,15 @@
-import React from "react"
+import styles from './index.less'
+import {useLocation, useHistory} from 'umi'
 
 export default (props: any) => {
-  return <div>
-    404 page.
+  const l = useLocation()
+  const h = useHistory()
+  return <div className={styles.errorPage}>
+    <span>噢, 页面 <span>{l.pathname}</span> 没找着 {':('}</span>
+    <br />
+    <a onClick={(e) => {
+      e.preventDefault()
+      h.replace('/')
+    }}>返回主页 {'>'} </a>
   </div>
 }
