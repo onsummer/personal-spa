38865d20cbf59482087599bb8171d0d9042b2def
Gypsophila <onsummer@foxmail.com>
2021-09-22 11:20:21 +0800

    [update] 备案号

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,19 +1,27 @@
-import { } from 'react'
+import {} from 'react'
 import { useHistory } from 'umi'
 
 import styles from './index.less'
 
 export default () => {
-
   const h = useHistory()
 
-  return <div className={styles.homeRoot}>
-    <div className={styles.homeContent}>
-      <h1> 欢迎光临 {":)"} </h1>
-      <h2 onClick={() => {
-        h.push('/map2d')
-      }}> 应用: Map2D </h2>
+  return (
+    <div className={styles.homeRoot}>
+      <div className={styles.homeContent}>
+        <h1> 欢迎光临 {':)'} </h1>
+        <h2
+          onClick={() => {
+            h.push('/map2d')
+          }}
+        >
+          {' '}
+          应用: Map2D{' '}
+        </h2>
+      </div>
+      <a href="https://beian.miit.gov.cn/" rel="nofollow noopener">
+        粤ICP备 2021103683 号
+      </a>
     </div>
-    <a href="https://beian.miit.gov.cn/" rel="nofollow noopener">粤ICP备 2021103683 号 - 1</a>
-  </div>
+  )
 }
